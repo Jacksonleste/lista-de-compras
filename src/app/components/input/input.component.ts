2f6e32d8bfed61a89be8e234be00184d0cc5b0fd
Jackson Leste <jacksongoncalves.leste@gmail.com>
2024-11-01 16:21:45 -0300

    feat: adicionando novos itens

--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -1,15 +1,29 @@
+import { ListaDeCompraService } from './../../service/lista-de-compra.service';
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-input',
   standalone: true,
-  imports:[],
+  imports: [FormsModule],
   templateUrl: './input.component.html',
-  styleUrls: ['./input.component.css']
+  styleUrls: ['./input.component.css'],
 })
 export class InputComponent implements OnInit {
+  item: string = '';
 
-  constructor() { }
+  constructor(private listaDeCompraService: ListaDeCompraService) {}
 
-  ngOnInit(): void { }
+  ngOnInit(): void {}
+
+  salvarItem() {
+    console.log('salvarItem');
+    if(this.item.length < 0) return;
+    this.listaDeCompraService.salvarItem(this.item);
+    this.limpaCampo();
+  }
+
+  limpaCampo() {
+    this.item = '';
+  }
 }
--- src/app/service/lista-de-compra.service.ts
+++ src/app/service/lista-de-compra.service.ts
@@ -2,36 +2,52 @@ import { Item } from '../interfaces/iItem';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ListaDeCompraService {
-
   private listaDeCompra: Item[] = [
     {
-      "id": 1,
-      "nome": "Queijo prato",
-      "data": "Segunda-feira (31/10/2022) às 08:30",
-      "comprado": false
+      id: 1,
+      nome: 'Queijo prato',
+      data: 'Segunda-feira (31/10/2022) às 08:30',
+      comprado: false,
     },
     {
-      "id": 2,
-      "nome": "Leite integral",
-      "data": "Segunda-feira (31/10/2022) às 08:30",
-      "comprado": false
+      id: 2,
+      nome: 'Leite integral',
+      data: 'Segunda-feira (31/10/2022) às 08:30',
+      comprado: false,
     },
     {
-      "id": 3,
-      "nome": "Mamão papaia",
-      "data": "Segunda-feira (31/10/2022) às 08:30",
-      "comprado": true
+      id: 3,
+      nome: 'Mamão papaia',
+      data: 'Segunda-feira (31/10/2022) às 08:30',
+      comprado: true,
     },
-  ]
+  ];
 
   constructor() {
     console.log('Instanciando dependências necessárias para o serviço.');
   }
 
-  getListaDeCompra(){
+  getListaDeCompra() {
     return this.listaDeCompra;
+  }
+
+  criarItem(nomeItem: string): Item {
+    const id = this.listaDeCompra.length + 1;
+    return {
+      id: id,
+      nome: nomeItem,
+      data: new Date().toLocaleDateString('pt-BR'),
+      comprado: false,
+    };
+  }
+
+  salvarItem(nomeItem: string) {
+    const item:Item = this.criarItem(nomeItem);
+    console.log(item);
+    this.listaDeCompra.push(item);
+    console.log(this.listaDeCompra);
   }
 }
